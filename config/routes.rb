7b23a6e1d4fc648d5366e7ef2ee2de9e07cc6000
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-04-21 23:43:30 -0700

    catch all for frontend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
